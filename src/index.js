6a1e37126e97abfcb05b129685e4ed4fea50ca10
kaali04 <ofcanubhav@gmail.com>
2025-10-05 12:27:11 +0530

    .

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed", err);
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
